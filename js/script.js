76d3aec2775e165f71c6d345cdf62e864e26db27
collincates <collin.cates@gmail.com>
2019-03-16 14:29:53 -0700

    More CSS styling, order tickets line up correctly now

--- js/script.js
+++ js/script.js
@@ -1,114 +1,3 @@
-function startGame() {
-  randomizeIngredientImages();
-
-}
-
-const startButton = document.getElementById('start-button');
-startButton.addEventListener('click', randomizeIngredientImages);
-
-
-function shuffle(array) {
-    let currentIndex = array.length;
-    let temporaryValue, randomIndex;
-
-    while (0 != currentIndex) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-    return array;
-}
-
-
-// function to randomly place ingredients
-// should 'know' which ingredients are needed for the upcoming recipe(s) in the pipeline
-// and place those definitely, then fill the remaining slots with random stuff
-
-// the above function is called after a few recipes are done, more specifically,
-// after all of the NEEDED ingredients are used on the recipes
-
-
-// The ingredient images should appear in a different order every time
-// the game is played. This function uses the custom function shuffle(array)
-// to ensure this randomization. randomizeIngredientImages is called at
-// the beginning of each game.
-function randomizeIngredientImages() {
-    const ingredientImageDivs = document.getElementsByClassName('ingredient-image-container');
-    let shuffledIngredients = shuffle(ingredients.slice());
-
-    for (i = 0; i < ingredientImageDivs.length; i++) {
-        let image = document.createElement('img');
-        image.name = shuffledIngredients[i].name;
-        image.src = shuffledIngredients[i].imageLink;
-        image.style.maxHeight = '100%';
-        image.style.maxWidth = '100%';
-        ingredientImageDivs[i].appendChild(image);
-        ingredientImageDivs[i].addEventListener('click', crossOffItem);
-    }
-
-}
-
-
-let activeOrderTicket = document.getElementById('order-ticket-active');
-let orderIngredientsList = activeOrderTicket.children[1].children;
-
-// function to click on ingredient image.
-function crossOffItem(event) {
-    let clickedIngredient = event.target;
-
-    for (i = 0; i < orderIngredientsList.length; i++) {
-        if (orderIngredientsList[i].textContent === clickedIngredient.name) {
-            orderIngredientsList[i].style.textDecoration = 'line-through wavy black';
-            orderIngredientsList[i].style.backgroundColor = 'gray';
-        }
-    }
-}
-
-
-const orderTicketsDiv = document.getElementById('order-tickets');
-
-function createRandomOrderTicket() {
-    let shuffledOrderTickets = shuffle(orderTicketData.slice());
-    let currentTicket = shuffledOrderTickets.pop();
-
-    let orderTicket = document.createElement('div');
-    orderTicket.className = 'order-ticket';
-
-    let h3 = document.createElement('h3');
-    h3.textContent = currentTicket.name;
-    orderTicket.appendChild(h3);
-
-    let ul = document.createElement('ul');
-    ul.className = 'recipe-items';
-    orderTicket.appendChild(ul);
-
-    for (i = 0; i < currentTicket.ingredients.length; i++) {
-        let li = document.createElement('li')
-        li.textContent = currentTicket.ingredients[i];
-        ul.appendChild(li);
-    }
-
-    orderTicketsDiv.appendChild(orderTicket);
-}
-
-
-// make active order ticket
-// orderTicket.className = 'order-ticket-active';
-
-// function to check when all items on recipe list are checked off,
-
-// remove recipe from order tickets
-
-// make next recipe in array the order-ticket-active id
-
-// generate new recipe at end of array
-
-// slide all recipes over
-
-
 const dishes = [
     {
     name: 'Burger',
@@ -119,7 +8,6 @@ const dishes = [
     imageLink: 'https://openclipart.org/download/309325/1541081610.svg',
     },
 ]
-
 const orderTicketData = [
   {
     name: 'Cheeseburger',
@@ -175,7 +63,6 @@ const tools = [
     imageLink: 'https://openclipart.org/download/28810/purzen-Cartoon-spatula.svg',
     }
 ]
-
 const ingredients = [
     {
     name: 'Bread',
@@ -222,7 +109,7 @@ const ingredients = [
     imageLink: 'https://openclipart.org/download/23677/papapishu-Fried-egg.svg',
     },
     {
-    name: 'Veggie patty',
+    name: 'Veggie Patty',
     imageLink: 'https://openclipart.org/download/209651/food-potato-patty.svg',
     },
     {
@@ -234,16 +121,10 @@ const ingredients = [
     imageLink: 'https://openclipart.org/download/291387/radishes.svg',
     },
     {
-    name: 'French fries',
+    name: 'French Fries',
     imageLink: 'https://openclipart.org/download/314326/1548611385.svg',
     },
 ]
-
-    // {
-    // name: '',
-    // imageLink: '',
-    // },
-
 const condiments = [
     {
     name: 'Ketchup',
@@ -266,3 +147,112 @@ const condiments = [
     imageLink: '',
     },
 ]
+const orderTicketsDiv = document.getElementById('order-tickets');
+
+function startGame() {
+  randomizeIngredientImages();
+
+}
+
+const startButton = document.getElementById('start-button');
+startButton.addEventListener('click', randomizeIngredientImages);
+
+
+function shuffle(array) {
+    let currentIndex = array.length;
+    let temporaryValue, randomIndex;
+
+    while (0 != currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
+
+
+// function to randomly place ingredients
+// should 'know' which ingredients are needed for the upcoming recipe(s) in the pipeline
+// and place those definitely, then fill the remaining slots with random stuff
+
+// the above function is called after a few recipes are done, more specifically,
+// after all of the NEEDED ingredients are used on the recipes
+
+
+// The ingredient images should appear in a different order every time
+// the game is played. This function uses the custom function shuffle(array)
+// to ensure this randomization. randomizeIngredientImages is called at
+// the beginning of each game.
+function randomizeIngredientImages() {
+    const ingredientImageDivs = document.getElementsByClassName('ingredient-image-container');
+    let shuffledIngredients = shuffle(ingredients.slice());
+
+    for (i = 0; i < ingredientImageDivs.length; i++) {
+        let image = document.createElement('img');
+        image.name = shuffledIngredients[i].name;
+        image.src = shuffledIngredients[i].imageLink;
+        image.style.maxHeight = '100%';
+        image.style.maxWidth = '100%';
+        ingredientImageDivs[i].appendChild(image);
+        ingredientImageDivs[i].addEventListener('click', crossOffItem);
+    }
+
+}
+
+
+function createRandomOrderTicket() {
+    let shuffledOrderTickets = shuffle(orderTicketData.slice());
+    let currentTicket = shuffledOrderTickets.pop();
+
+    let orderTicket = document.createElement('div');
+    orderTicket.className = 'order-ticket';
+
+    let h3 = document.createElement('h3');
+    h3.textContent = currentTicket.name;
+    orderTicket.appendChild(h3);
+
+    let ul = document.createElement('ul');
+    ul.className = 'recipe-items';
+    orderTicket.appendChild(ul);
+
+    for (i = 0; i < currentTicket.ingredients.length; i++) {
+        let li = document.createElement('li')
+        li.textContent = currentTicket.ingredients[i];
+        ul.appendChild(li);
+    }
+
+    orderTicketsDiv.appendChild(orderTicket);
+}
+
+// function to click on ingredient image.
+function crossOffItem(event) {
+    let clickedIngredient = event.target;
+    let activeOrderTicket = document.getElementById('active-ticket');
+    let orderIngredientsList = activeOrderTicket.children[1].children;
+
+    for (i = 0; i < orderIngredientsList.length; i++) {
+        if (orderIngredientsList[i].textContent === clickedIngredient.name) {
+            orderIngredientsList[i].style.textDecoration = 'line-through wavy black';
+            orderIngredientsList[i].style.backgroundColor = 'gray';
+        }
+    }
+}
+
+while (orderTicketsDiv.children.length > 0) {
+    orderTicketsDiv.children[0].setAttribute('id', 'active-ticket');
+}
+// make active order ticket
+// orderTicket.className = 'order-ticket active-ticket';
+
+// function to check when all items on recipe list are checked off,
+
+// remove recipe from order tickets
+
+// make next recipe in array the order-ticket active-ticket class
+
+// generate new recipe at end of array
+
+// slide all recipes over
